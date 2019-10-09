83e9c7a408b8b7599baa4372e1059123929aa466
xiaoshiguang123 <xyt123@163.com>
2019-10-09 16:38:41 +0800

    添加图标库

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,11 +4,14 @@
         <button @click="userLogin('true')">点击改变userLogin:{{$store.state.isLogin}}</button>
         <button @click="yh('22')">点击改变yhid:{{$store.state.yhid}}</button>
         <h3>{{yhid}}</h3>
+          <!-- element-ui引入测试 -->
           <el-switch
             v-model="value"
             active-color="#13ce66"
             inactive-color="#ff4949">
-          </el-switch>        
+          </el-switch>
+          <!-- 图标库引入测试 -->
+          <i class="iconfont iconjiaohuan2"></i>        
   </div>
 </template>
 
