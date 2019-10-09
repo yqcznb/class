f7a9fe4bb018040fa1512aba37658375d0cb8dae
xiaoshiguang123 <xyt123@163.com>
2019-10-09 16:28:52 +0800

    添加axios和element-ui

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,6 +4,11 @@
         <button @click="userLogin('true')">点击改变userLogin:{{$store.state.isLogin}}</button>
         <button @click="yh('22')">点击改变yhid:{{$store.state.yhid}}</button>
         <h3>{{yhid}}</h3>
+          <el-switch
+            v-model="value"
+            active-color="#13ce66"
+            inactive-color="#ff4949">
+          </el-switch>        
   </div>
 </template>
 
@@ -13,7 +18,8 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App',
+      value: true
     }
   },
   computed:{
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 import App from './App'
 import router from './router'
 import store from './store'
@@ -10,6 +12,7 @@ Vue.prototype.$axios = axios;
 import QS from 'qs'
 Vue.prototype.qs = QS;
 Vue.config.productionTip = false
+Vue.use(ElementUI);
 
 /* eslint-disable no-new */
 new Vue({
