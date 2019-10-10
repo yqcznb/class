804721ca36a48b437fb3c42260298d9517b2a2b9
hjxw <1367464114@qq.com>
2019-10-10 09:59:36 +0800

    dev-hjw --10.10--2

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import Router from 'vue-router
+import Router from 'vue-router'
 import stu_index from '@/components/stu_index/stu_index'
 import index from '@/components/index'
 import login from '@/components/login/login'
@@ -17,6 +17,11 @@ export default new Router({
       path: '/index',
       name: 'index',
       component: index
+    },
+    {
+      path: '/stu_index',
+      name: 'stu_index',
+      component: stu_index
     }
   ]
 })
