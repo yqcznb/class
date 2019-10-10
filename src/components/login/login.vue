f6ea80bfccc51c6c6449c373a088dbd4d3eb1708
F-nothing <babalalw@gmail.com>
2019-10-10 21:39:49 +0800

    axiba10102139--5

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -1,10 +1,22 @@
 <template>
     <div id="login">
-        <img src="https://images.weserv.nl/?url=https://c-ssl.duitang.com/uploads/item/201910/10/2019101092532_kinVR.png" alt="" class="login_backimg" ref="login_backimg">
         <div class="login_box">
-            <img src="https://images.weserv.nl/?url=https://upload-images.jianshu.io/upload_images/19325457-ced61ba4e3cf881c.png" alt="stu">
-            <div class="img_box">
-                
+            <div class="login_img_box">
+                <div id="img_box_up">
+                    <img src="https://images.weserv.nl/?url=https://upload-images.jianshu.io/upload_images/19325457-cb8e32f88e62ad95.png" alt="stu" class="img_choose">
+                    <img src="https://images.weserv.nl/?url=https://upload-images.jianshu.io/upload_images/19325457-ced61ba4e3cf881c.png" alt="stu" class="img_choose">
+                </div>
+                <div id="img_box_down">
+                    <img src="https://images.weserv.nl/?url=https://upload-images.jianshu.io/upload_images/19325457-326cefd8fc8139db.png" alt="stu" class="img_choose">
+                </div>
+            </div>
+            <div class="login_input_box">
+                <div class="login_input_count">
+                    <input type="text" name="" id="input_count">
+                </div>
+                <div class="login_input_pass">
+
+                </div>
             </div>
         </div>
     </div>
@@ -14,53 +26,15 @@ export default {
     name: 'login',
     data () {
         return {
-            wind_width: window.innerWidth,
-            wind_heigt: window.innerHeight,
-            wind_size: window.innerWidth+window.innerHeight,
-            backimg_width: '',
-            backimg_height: '',
 
         }
     },
-    mounted() {
-        let backimg_dom = this.$refs.login_backimg;
-        this.backimg_width = backimg_dom.offsetWidth;
-        this.backimg_height= backimg_dom.offsetHeight;
-
-        if((this.backimg_width / this.backimg_height) > (this.wind_width / this.wind_heigt)) {
-            backimg_dom.style.height = '100%';
-        }
-        else {
-            backimg_dom.style.width = '100%';
-        }
+    // mounted() {
         
-        window.onresize = () => {
-            return(() => {
-                window.wind_size = window.innerWidth + window.innerHeight;
-                window.wind_width = window.innerWidth;
-                window.wind_heigt = window.innerHeight;
-                this.wind_size = window.wind_size;
-                this.wind_width = window.wind_width;
-                this.wind_heigt = window.wind_heigt;
-            })()
-        };
+    // },
+    // watch: {
 
-    },
-    watch: {
-        'wind_size':function(val) {
-            this.wind_size = val;
-            let backimg_dom = this.$refs.login_backimg;
-            backimg_dom.style.width = 'auto';
-            backimg_dom.style.height = 'auto';
-            if((this.backimg_width / this.backimg_height) > (this.wind_width / this.wind_heigt)) {
-                backimg_dom.style.height = '100%';
-            }
-            else {
-                backimg_dom.style.width = '100%';
-            }
-        },
-
-    },
+    // },
     methods: {
 
     }
@@ -74,11 +48,46 @@ export default {
         margin: 0;
         overflow: hidden;
     }
-    .login_backimg {
-        position: absolute;
-        left: 0;
-        right: 0;
+    .login_box {
+        border: 1px solid red;
+        width: 40.5ex;
         margin: 0 auto;
-        z-index: -1;
+    }
+    .login_img_box {
+        width: 100%;
+        height: 37ex;
+        margin: 3ex auto;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        /* border: 1px solid red; */
+    }
+    #img_box_up,#img_box_down {
+        /* border: 1px solid blue; */
+        width: 100%;
+        margin: 0 auto;
+    }
+    #img_box_up {
+        display: flex;
+        justify-content: space-between;
+    }
+    .img_choose {
+        width: 10em;
+    }
+    .login_input_box {
+        width: 75%;
+        /* height: 10em; */
+        margin: 0 auto;
+        border: 1px solid blue;
+    }
+    .login_input_count {
+        width: 100%;
+        height: 2em;
+    }
+    #input_count {
+        width: 100%;
+        height: 100%;
+        border: 0;
+        margin: 0;
     }
 </style>
