eac3b2e6ef4a9616be214b9bc890540612acb5e9
F-nothing <babalalw@gmail.com>
2019-10-11 20:54:44 +0800

    axiba10112054--3

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -1,13 +1,18 @@
 <template>
     <div id="login">
+        <span class="record_info" v-text="record_info"></span>
         <div class="login_box">
             <div class="login_img_box">
                 <div id="img_box_up">
                     <transition 
                      enter-active-class="animated bounceInDown" leave-active-class="animated bounceOutUp">
                         <div class="up_show" v-show="!login_img_click">
-                            <img :src="login_img_ul" class="img_choose" @click="change_login_profess(login_img_ul)">
-                            <img :src="login_img_ur" class="img_choose" @click="change_login_profess(login_img_ur)">
+                            <el-tooltip class="item" effect="dark" :content="login_content_ul" placement="left">
+                                <img :src="login_img_ul" class="img_choose" @click="change_login_profess(login_img_ul)">
+                            </el-tooltip>
+                            <el-tooltip class="item" effect="dark" :content="login_content_ur" placement="right">
+                                <img :src="login_img_ur" class="img_choose" @click="change_login_profess(login_img_ur)">
+                            </el-tooltip>
                         </div>
                     </transition>
                     <div class="up_hide" v-show="login_img_click">
@@ -18,7 +23,9 @@
                 <div id="img_box_down">
                     <transition 
                      enter-active-class="animated fadeIn" leave-active-class="animated fadeOut">
-                        <img :src="login_img_down" class="img_choose" @click="login_img_click = !login_img_click">
+                        <el-tooltip class="item" effect="dark" :content="login_content_down" placement="right" hide-after=0 value=true>
+                            <img :src="login_img_down" class="img_choose" @click="login_img_click = !login_img_click">
+                        </el-tooltip>
                     </transition>
                 </div>
             </div>
@@ -38,13 +45,17 @@ export default {
     name: 'login',
     data () {
         return {
+            record_info: '备案信息',
             // 职业选择
             login_img_ul: 'https://upload-images.jianshu.io/upload_images/19325457-cb8e32f88e62ad95.png',
             login_img_ur: 'https://upload-images.jianshu.io/upload_images/19325457-ced61ba4e3cf881c.png',
             login_img_down: 'https://upload-images.jianshu.io/upload_images/19325457-326cefd8fc8139db.png',
+            login_content_ul: '我是老师',
+            login_content_ur: '我是学生',
+            login_content_down: '我是管理员',
 
             login_img_click: true,
-            skip_link: '/zujian',
+            skip_link: '/gzujian',
         }
     },
     // mounted() {
@@ -58,16 +69,33 @@ export default {
             let login_img_ul = this.login_img_ul;
             let login_img_ur = this.login_img_ur;
             let login_img_down = this.login_img_down;
+
+            let login_content_ul = this.login_content_ul;
+            let login_content_ur = this.login_content_ur;
+            let login_content_down = this.login_content_down;
+
             if(url == login_img_ul) {
+                // 图片切换
                 this.login_img_down = url;
                 this.login_img_ul = login_img_ur;
                 this.login_img_ur = login_img_down;
+                // 职称切换
+                this.login_content_down = login_content_ul;
+                this.login_content_ul = login_content_ur;
+                this.login_content_ur = login_content_down;
+
                 this.skip_link = '/tch_index';
             }
             else if(url == login_img_ur) {
+                // 图片切换
                 this.login_img_down = url;
                 this.login_img_ur = login_img_ul;
                 this.login_img_ul = login_img_down;
+                // 职称切换
+                this.login_content_down = login_content_ur;
+                this.login_content_ur = login_content_ul;
+                this.login_content_ul = login_content_down;
+
                 this.skip_link = '/stu_index';
             }
             else {}
@@ -91,6 +119,14 @@ export default {
     }
     img {
         cursor: pointer;
+    }
+    .record_info {
+        width: 10em;
+        line-height: 4ex;
+        display: inline-block;
+        border-radius: 0 0 3ex 3ex;
+        background-color: rgba(0, 0, 0, 0.7);
+        color: white;
     }
     .login_box {
         width: 45.5ex;
