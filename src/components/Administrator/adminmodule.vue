3d1a26f44f387304d231bcd6b870381503a272a3
1119834840 <1119834840@qq.com>
2019-10-24 10:05:15 +0800

    test2

--- src/components/Administrator/adminmodule.vue
+++ src/components/Administrator/adminmodule.vue
@@ -1,14 +1,14 @@
 <template>
-    <div class="admin_module">
+    <div class="admin_settings">
         <div class="line_bar">
-            <hr> <span class="module_title">上传专业分方向信息表</span><hr>
+            <hr> <span class="settings_title">上传专业分方向信息表</span> <hr>
         </div>
-        <div class="module_download">
-            <div class="module_update_pass" @click = "set_up_pass_confirm">
-                <span class="iconfont iconxiazaimokuai"></span> <span class="set_up_pass_title">下载模块</span>
+        <div class="settings_choose">
+            <div class="settings_download_pass" @click = "set_up_pass_confirm">
+                <span class="iconfont iconiconfontxiazai"></span> <span class="set_up_pass_title">下载模块</span>
             </div>
-            <div class="module_upload" @click="set_sign_confirm">
-                <span class="iconfont iconshangchuanxinxi"></span> <span class="set_sign_title">上传信息</span>
+            <div class="settings_upload" @click="set_sign_confirm">
+                <span class="iconfont iconiconfontshangchuan"></span> <span class="set_sign_title">上传信息</span>
             </div>
         </div>
     </div>
@@ -31,44 +31,52 @@ export default {
         
     // },
     methods: {
-    },
+        
+        },
+        
 }
 </script>
 <style lang="scss" scoped>
-.admin_module{
+.admin_settings{
     position: relative;
     width: 100%;
     height: 100%;
     // padding: 7ex;
-    .line_bar,.module_choose {
+    .line_bar,.settings_choose {
         min-width: 330px;
         display: flex;
         margin: 2em auto;
         margin-bottom: 10em;
         justify-content: space-between;
         align-items: center;
-        .module_title {
-            font-size: 2em;
-            color: white;
+        hr{
+            border:none;
+            background-color: rgba(0,0,0,0);
         }
-        .module_update_pass,.module_upload {
+        .settings_title {
+            font-size: 3em;
+            color: white;
+            letter-spacing: 5px;
+        }
+        .settings_download_pass,.settings_upload {
             display: flex;
             cursor: pointer;
             flex-direction: column;
             align-items: center;
             color: white;
-            .iconxiazaimokuai,.iconshangchuanxinxi{
-                font-size: 11em;
+            .iconiconfontshangchuan,.iconiconfontxiazai {
+                font-size: 20em;
             }
             .set_up_pass_title,.set_sign_title {
                 font-size: 4ex;
+                color:white;
             }
         }
     }
     .line_bar {
         width: 55%;
     }
-    .module_choose {
+    .settings_choose {
         width: 47%;
     }
    
